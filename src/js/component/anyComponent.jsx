d04fa280385035032ec56c537f7c664cc5ebd87c
javier-bustillo <jjavier.bustillo@gmail.com>
2022-06-19 08:56:20 +0000

    commit

---
+++ src/js/component/anyComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnyComponent = ({ state }) => {
+  return (
+    <div className="container">
+      <p>{state}</p>
+    </div>
+  );
+};
+
+export default AnyComponent;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import AnyComponent from "./anyComponent.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [state, setState] = useState("");
+
+  function handleState(e) {
+    setState(e.target.value);
+  }
+
+  function handleSubmit() {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Your task here"
+          aria-label="Your task here"
+          aria-describedby="basic-addon1"
+          onChange={handleState}
+        />
+        <AnyComponent state={state} />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Home;
