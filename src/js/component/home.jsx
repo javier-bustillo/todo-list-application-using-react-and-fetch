5dae63e8f7f61750ecde3d6cdecf2a796cd58881
javier-bustillo <jjavier.bustillo@gmail.com>
2022-06-30 10:00:38 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,13 +9,17 @@ const Home = () => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* setea el valor de ...inputList con los valores de input */ 
-    /* inputList lista de states */ 
+    /* setea el valor de ...inputList con los valores de input */
+    /* inputList lista de states */
     setInputList([...inputList, input]);
   };
 
+  const handleButton = (index) => () =>
+    setInputList((items) => items.filter((_, i) => i !== index));
+
   return (
     <form onSubmit={handleSubmit}>
+      <h1 className="m-5 d-flex justify-content-center">Todo List</h1>
       <div className="container">
         <input
           type="text"
@@ -26,17 +30,25 @@ const Home = () => {
           onChange={handleState}
         />
         {/* representa de manera gráfica los datos con los valores de inputList */}
-        <div className="container bg-secondary">
-          <ul className="list-group">
+        <div className="container">
+          <ul className="list-group my-5">
             {inputList.map(function (currentValue, index) {
               return (
-                <li key={index} className="list-group-item">
+                <li key={index} className="list-group-item position-relative">
                   {currentValue}
+
+                  <button
+                    type="button"
+                    className="btn-close position-absolute top-50 end-0 translate-middle"
+                    aria-label="Close"
+                    onClick={handleButton(index)}
+                  ></button>
                 </li>
               );
             })}
           </ul>
         </div>
+        <p className="mt-3">{inputList.length} items left</p>
       </div>
     </form>
   );
